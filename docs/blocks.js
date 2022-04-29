6e6e868a948dc5ee37fa538c6e95f12df6e45b52
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2022-04-29 11:58:04 +0200

    basic blocks

--- docs/blocks.js
+++ docs/blocks.js
@@ -1,3 +1,38 @@
+const defineBlock = (config) => ({
+    init: function() {
+        this.jsonInit(config);
+    }
+});
+
+Blockly.Blocks.actions_turn = defineBlock({
+    message0: 'otoč se',
+    previousStatement: null,
+    nextStatement: null,
+    colour: 110,
+});
+
+Blockly.Blocks.actions_jump = defineBlock({
+    message0: 'skoč %1',
+    args0: [{name: 'TYPE', type: 'field_dropdown', options: [['vysoko', 'HIGH'], ['daleko', 'LONG']]}],
+    previousStatement: null,
+    colour: 110,
+})
+
 export const toolbox = {
-    contents: []
+    contents: [{
+        kind: 'block',
+        type: 'actions_turn',
+    }, {
+        kind: 'block',
+        type: 'actions_jump',
+        fields: {
+            TYPE: 'HIGH',
+        },
+    }, {
+        kind: 'block',
+        type: 'actions_jump',
+        fields: {
+            TYPE: 'LONG',
+        },
+    }],
 }
