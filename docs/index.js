2d6a82e81dfaca3a8560a76da57c9819f1ed4afc
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2022-05-03 12:59:21 +0200

    loading assets

--- docs/index.js
+++ docs/index.js
@@ -9,9 +9,25 @@ document.getElementById('reset').addEventListener('click', () => {
 });
 
 const gameElement = document.getElementById('game')
+const width = gameElement.offsetWidth;
+const height = gameElement.offsetHeight;
+
 new Phaser.Game({
     type: Phaser.AUTO,
     parent: gameElement,
-    width: gameElement.offsetWidth,
-    height: gameElement.offsetHeight,
+    width,
+    height,
+    scene: {
+        preload,
+        create,
+    }
 });
+
+function preload() {
+    this.load.image('background', 'images/blue_grass.png');
+    this.load.atlasXML('sprites', 'images/spritesheet_complete.png', 'images/spritesheet_complete.xml');
+}
+
+function create() {
+    this.add.image(width/2, height/2, 'background').setDisplaySize(width, height);
+}
