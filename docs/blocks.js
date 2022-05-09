3bc20fd9269706ddba77ffd32da7a10dc32d1660
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2022-05-09 11:40:44 +0200

    toolbox update

--- docs/blocks.js
+++ docs/blocks.js
@@ -4,35 +4,42 @@ const defineBlock = (config) => ({
     }
 });
 
+const ACTION_COLOUR = 110;
+
 Blockly.Blocks.actions_turn = defineBlock({
     message0: 'otoč se',
     previousStatement: null,
     nextStatement: null,
-    colour: 110,
+    colour: ACTION_COLOUR,
 });
 
 Blockly.Blocks.actions_jump = defineBlock({
     message0: 'skoč %1',
     args0: [{name: 'TYPE', type: 'field_dropdown', options: [['vysoko', 'HIGH'], ['daleko', 'LONG']]}],
     previousStatement: null,
-    colour: 110,
+    colour: ACTION_COLOUR,
 })
 
 export const toolbox = {
     contents: [{
-        kind: 'block',
-        type: 'actions_turn',
-    }, {
-        kind: 'block',
-        type: 'actions_jump',
-        fields: {
-            TYPE: 'HIGH',
-        },
-    }, {
-        kind: 'block',
-        type: 'actions_jump',
-        fields: {
-            TYPE: 'LONG',
-        },
+        kind: 'category',
+        name: 'Akce',
+        colour: ACTION_COLOUR,
+        contents: [{
+            kind: 'block',
+            type: 'actions_turn',
+        }, {
+            kind: 'block',
+            type: 'actions_jump',
+            fields: {
+                TYPE: 'HIGH',
+            },
+        }, {
+            kind: 'block',
+            type: 'actions_jump',
+            fields: {
+                TYPE: 'LONG',
+            },
+        }]
     }],
 }
